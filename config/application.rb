8eb6d162b7c4b8106dd2871eef6e9383e7e1064d
Damian143 <damian_irarrazaval@hotmail.com>
2015-10-14 15:31:25 -0300

    comment assets precompile

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Prestamelo
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     #Devise
-    config.assets.initialize_on_precompile = false
+
 
   end
 end
