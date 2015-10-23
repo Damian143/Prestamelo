07b3909d7dd19291ba8bd2559830c297b07cfc5e
Damian143 <damian_irarrazaval@hotmail.com>
2015-10-23 12:59:16 -0300

    bxbuilder and fontaweesome gems setup

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.paths << Rails.root.join('/app/assets/fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( js/jquery.matchHeight-min.js )
+  
